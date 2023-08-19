e1bc3acf274436a8f64c405e3b55fcb1c14e9fe6
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2023-08-18 19:44:46 -0500

    Avance 4

--- src/config/global.js
+++ src/config/global.js
@@ -93,7 +93,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/anexos.zip',
+        download: 'downloads/921200_CF08_DU.zip',
       },
       {
         icono: 'fas fa-download',
